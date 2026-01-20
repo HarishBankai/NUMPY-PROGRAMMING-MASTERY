ac554b9d52e9e30c3b894469a8b9be40dd00b2fe
HarishBankai <harishambalgikar@gmail.com>
2026-01-20 20:55:48 +0530

    Add comments in the code

--- Indexing Array.py
+++ Indexing Array.py
@@ -1,8 +1,14 @@
 import numpy as np  
 
+#Creating a numpy array
 arr = np.array([1, 2, 4, 9, 7, 3])
 
+#Printing the array
 print(arr)
 
+#Accessing elements using indexing
+#Indexing starts from 0
 print("First elements : ", arr[0])
+
+#Accessing the fourth element
 print("Fourth elements : ",arr[3])
