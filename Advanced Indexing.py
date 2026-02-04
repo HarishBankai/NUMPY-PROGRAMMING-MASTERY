65d0dac147177901a2a9f608eebf255796f0ad90
HarishBankai <harishambalgikar@gmail.com>
2026-02-04 19:40:57 +0530

    Add file via VS Code

--- Advanced Indexing.py
+++ Advanced Indexing.py
@@ -1,10 +1,10 @@
 import numpy as np  
 
-data = np.arange(50)
+data = np.arange(100)
 
 even_number = data[data % 2 == 0]
 odd_number = data[data % 2 != 0]
-
+	
 print("OG ARRAY : ", data)
 print("EVEN NUMBERS : ", even_number)
 print("ODD NUMBERS : ", odd_number)
