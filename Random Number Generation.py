1785f318b079f7eadfd218c10e218d8e51b56f3b
HarishBankai <harishambalgikar@gmail.com>
2026-02-04 18:49:59 +0530

    Add file via VS Code

---
+++ Random Number Generation.py
@@ -0,0 +1,7 @@
+import numpy as np  
+
+data = np.random.rand(2,2)
+print("Generation number in 0 and 1 in 2D array : ", data)
+
+normaldata = np.random.rand(10)
+print("Random sample for normal distribution : ", normaldata)
