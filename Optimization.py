70a0ba3cb8edd291da3db589476b4681d082e7e1
HarishBankai <harishambalgikar@gmail.com>
2026-02-04 19:51:54 +0530

    Add files via VS Code

---
+++ Optimization.py
@@ -0,0 +1,17 @@
+import numpy as np  
+
+data = np.random.rand(100000)  # Create an array of 100,000 random numbers using NumPy
+
+def sum_with_loop(data):  
+    """This function calculates the sum of elements in data using a Python loop."""
+    total = 0  
+    for num in data:  
+        total += num  
+    return total  
+
+def sum_with_numpy(data):  
+    """This function calculates the sum of elements in data using NumPy's optimized functions."""  
+    return np.sum(data)  
+
+print("SUM WITH LOOP : ", sum_with_loop(data))  
+print("SUM WITH NUMPY : ", sum_with_numpy(data))  
